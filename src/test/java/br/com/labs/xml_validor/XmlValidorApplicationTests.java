05af6b64ce88a57793185d8a7f80ae74c02e92ab
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-02-21 12:10:30 -0300

    updt

---
+++ src/test/java/br/com/labs/xml_validor/XmlValidorApplicationTests.java
@@ -0,0 +1,28 @@
+package br.com.labs.xml_validor;
+
+import br.com.labs.xml_validor.validation.Validation;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class XmlValidorApplicationTests {
+
+	@Test
+	public void givenValidXML_WhenIsValid_ThenTrue() throws IOException, SAXException {
+		assertTrue(new Validation("/home/resque/github/xml-validor/src/main/resources/templates/person.xsd",
+				"/home/resque/github/xml-validor/src/main/resources/templates/person.xml").isValid());
+	}
+
+	@Test
+	public void givenInvalidXML_WhenIsValid_ThenFalse() throws IOException, SAXException {
+		assertFalse(new Validation("/home/resque/github/xml-validor/src/main/resources/templates/person.xsd",
+				"/home/resque/github/xml-validor/src/main/resources/templates/person.xml").isValid());
+	}
+
+}
